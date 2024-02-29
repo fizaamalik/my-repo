92e3b474eda833b5f15094c8b9bb8a40c350cc71
Fiza Malik <fiza.malik@walkingtree.tech>
2024-02-29 11:35:43 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello from vscode");
+        System.out.println(new Date());
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
